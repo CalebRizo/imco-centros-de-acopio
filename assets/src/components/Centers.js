748d61d7eb114983f1d16591ea6ca0eeedfcfcdc
Caleb Rizo <kaleb.rizzo@gmail.com>
2018-06-13 00:19:06 -0500

    Connects phoenix centers api with react app

--- assets/src/components/Centers.js
+++ assets/src/components/Centers.js
@@ -1,49 +1,57 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Table, Button } from 'reactstrap'
+import axios from 'axios'
 
 class Centers extends React.Component {
   constructor() {
     super()
-    this.state = { languages: [], loading: true }
+    this.state = { centers: [], loading: true }
+  }
 
-    // Get the data from our API.
-    fetch('/api/gatheringCenters')
-      .then((response) => response.json())
-      .then((data) => {
-        this.setState({ languages: data.data, loading: false })
+  componentWillMount() {
+    this._fetchCenters()
+  }
+
+  _fetchCenters() {
+    axios.get('/api/centers')
+      .then(resp => {
+        const centers = resp.data.data
+        this.setState({ centers, loading: false })
       })
   }
 
-  _renderLanguagesTable(languages) {
+  _renderCentersTable() {
     return (
       <Table>
         <thead>
           <tr>
-            <th>Language</th>
-            <th>Example proverb</th>
+            <th>IMCO ID</th>
+            <th>Name</th>
+            <th>Address</th>
           </tr>
         </thead>
         <tbody>
-          {languages.map((language) =>
-            <tr key={language.id}>
-              <td>{language.name}</td>
-              <td>{language.proverb}</td>
-            </tr>
-          )}
+          {
+            this.state.centers.map(center =>
+              <tr key={center.id} style={{textAlign: 'center'}}>
+                <td>{center.imco_id}</td>
+                <td>{center.name}</td>
+                <td>{center.address}</td>
+              </tr>
+            )
+          }
         </tbody>
       </Table>
     )
   }
 
   render() {
-    const content = this.state.loading
-      ? <p><em>Loading...</em></p>
-      : this._renderLanguagesTable(this.state.languages)
+    const content = this.state.loading ? <p><em>Loading...</em></p> : this._renderCentersTable()
 
     return (
       <div>
-        <h1>Fetch Data</h1>
+        <h1>Fetch Centers Data</h1>
         <p>This component demonstrates fetching data from the Phoenix API endpoint.</p>
         {content}
         <br /><br />
