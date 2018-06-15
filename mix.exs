c0927a3cb8b8e5079230210158b2fbb0ba4ffbc2
Caleb Rizo <kaleb.rizzo@gmail.com>
2018-06-15 07:44:04 -0500

    Includes erlsom lib

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule ImcoCentrosDeAcopio.Mixfile do
   def application do
     [
       mod: {ImcoCentrosDeAcopio.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :erlsom]
     ]
   end
 
